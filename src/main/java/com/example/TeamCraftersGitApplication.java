05687c1052cd0dc36c2a08462fa2e8fadd2d9219
Sabyasachee Mohapatra <Sabyasachee.M@BRILLIO.COM>
2024-05-28 15:19:30 +0530

    first commit

---
+++ src/main/java/com/example/TeamCraftersGitApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCraftersGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCraftersGitApplication.class, args);
+	}
+
+}
